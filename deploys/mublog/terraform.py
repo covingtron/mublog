6752e030c43a934f14b930a24c2ad7743232809f
Coving Tron <255591504+covingtron@users.noreply.github.com>
2026-02-16 11:19:17 -0500

    Add Terraform flan file provisioner

--- deploys/mublog/terraform.py
+++ deploys/mublog/terraform.py
@@ -3,11 +3,29 @@
 from os import environ
 from pathlib import Path
 
-from helichopyter import provider, required_providers, resource
+from helichopyter import provider, provisioner, required_providers, resource, variable
 
 required_providers(cloudflare={'source': 'cloudflare/cloudflare', 'version': '5.14.0'})
 
 provider('cloudflare')
+variable('GIHA')(type='string')
+variable('TABR')(type='string')
+
+resource('terraform_data', 'flan')(
+    provisioner('local-exec')(
+        command=(
+            'mkdir -p ../../../site/ton && '
+            "printf 'CONA=mublog\\nENVI=%s\\nGIHA=%s\\nTABR=%s\\n' "
+            '"$TF_WORKSPACE" "$TF_VAR_GIHA" "$TF_VAR_TABR" > ../../../site/ton/flan'
+        ),
+        environment={
+            'TF_WORKSPACE': '${terraform.workspace}',
+            'TF_VAR_GIHA': '${var.GIHA}',
+            'TF_VAR_TABR': '${var.TABR}',
+        },
+    ),
+    triggers_replace=['${terraform.workspace}', '${var.GIHA}', '${var.TABR}'],
+)
 
 resource('cloudflare_workers_script', 'this')(
     account_id=environ['CLOUDFLARE_ACCOUNT_ID'],
